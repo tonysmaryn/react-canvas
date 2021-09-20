0c5597d359bfb77b373338638d211b61dec20482
Anton Maryn <tonysmaryn@gmail.com>
2021-09-20 20:23:06 +0300

    add delete button

--- src/components/RepeatCanvas/RepeatCanvas.jsx
+++ src/components/RepeatCanvas/RepeatCanvas.jsx
@@ -7,6 +7,18 @@ const RepeatCanvas = ({ images }) => {
   const insertHandler = () => {
     currentImage++;
     const context = canvasRef.current.getContext('2d');
+    loadImg(context);
+  };
+
+  const deleteHandler = () => {
+    currentImage--;
+    const context = canvasRef.current.getContext('2d');
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    loadImg(context);
+  };
+
+  const loadImg = (context) => {
     const img = new Image();
     img.src = images[currentImage];
     img.onload = () => context.drawImage(img, 0, 0);
@@ -16,6 +28,7 @@ const RepeatCanvas = ({ images }) => {
     <>
       <canvas width="600" height="900" ref={canvasRef} />
       <button onClick={() => insertHandler()}>Добавить</button>
+      <button onClick={() => deleteHandler()}>Удалить</button>
     </>
   );
 };
