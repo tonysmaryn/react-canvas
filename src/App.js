d2682c4b5e7dd3fad81476be442d8f4ca95b3f0c
Anton Maryn <tonysmaryn@gmail.com>
2021-09-20 20:05:40 +0300

    add insert button

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <DrawingCanvas addScreen={addScreen} images={images} />
-      <RepeatCanvas />
+      <RepeatCanvas images={images} />
     </>
   );
 }
--- src/components/RepeatCanvas/RepeatCanvas.jsx
+++ src/components/RepeatCanvas/RepeatCanvas.jsx
@@ -1,10 +1,21 @@
 import React, { useRef } from 'react';
 
-const RepeatCanvas = () => {
+const RepeatCanvas = ({ images }) => {
   const canvasRef = useRef(null);
+  let currentImage = 0;
+
+  const insertHandler = () => {
+    currentImage++;
+    const context = canvasRef.current.getContext('2d');
+    const img = new Image();
+    img.src = images[currentImage];
+    img.onload = () => context.drawImage(img, 0, 0);
+  };
+
   return (
     <>
       <canvas width="600" height="900" ref={canvasRef} />
+      <button onClick={() => insertHandler()}>Добавить</button>
     </>
   );
 };
