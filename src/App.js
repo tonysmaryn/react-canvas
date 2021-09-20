a685d5c342f1834e2646d629ec5eb1ccd81dbfb1
Anton Maryn <tonysmaryn@gmail.com>
2021-09-20 19:41:59 +0300

    add some style for canvas

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.scss';
 import DrawingCanvas from "./components/DrawingCanvas/DrawingCanvas";
 
 function App() {
--- src/components/DrawingCanvas/DrawingCanvas.jsx
+++ src/components/DrawingCanvas/DrawingCanvas.jsx
@@ -38,6 +38,8 @@ const DrawingCanvas = () => {
   return (
     <>
       <canvas
+        width="600"
+        height="900"
         ref={canvasRef}
         onMouseDown={startDrawing}
         onMouseUp={finishDrawing}
